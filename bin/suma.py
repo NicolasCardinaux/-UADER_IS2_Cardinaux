a6ee392e54e691a9bac7d39fefaee890a46cc74b
NicolasCardinaux <127860186+NicolasCardinaux@users.noreply.github.com>
2024-03-13 11:23:50 -0300

    suma

---
+++ bin/suma.py
@@ -0,0 +1,7 @@
+
+numero1 = input("Ingrese el primer número: ")
+numero2 = input("Ingrese el segundo número: ")
+numero1 = int(numero1)
+numero2 = int(numero2)
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
