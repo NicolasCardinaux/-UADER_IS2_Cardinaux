c9f05945c3ae737faef7ae11e50826be02f485d2
NicolasCardinaux <nicocardina01@gmail.com>
2024-03-20 21:38:57 -0300

    update

--- TP1/bin/suma.py
+++
@@ -1,7 +0,0 @@
-
-numero1 = input("Ingrese el primer número: ")
-numero2 = input("Ingrese el segundo número: ")
-numero1 = int(numero1)
-numero2 = int(numero2)
-suma = numero1 + numero2
-print("La suma de", numero1, "yy", numero2, "es:", suma)
