123ec98857fee6b408952b70721f997229c16ddb
NicolasCardinaux <nicocardina01@gmail.com>
2024-04-15 19:10:38 -0300

    Update punt_5.py

--- TP1-TP2-TP3-TP4/src/TP3/punt_5.py
+++ TP1-TP2-TP3-TP4/src/TP3/punt_5.py
@@ -1,3 +1,6 @@
+'''Extienda el ejemplo visto en el taller en clase de forma que se pueda utilizar para construir 
+aviones en lugar de vehículos. Para simplificar suponga que un avión tiene un “body”, 2 turbinas, 
+2 alas y un tren de aterrizaje.'''
 import os
 
 # Clase Director
