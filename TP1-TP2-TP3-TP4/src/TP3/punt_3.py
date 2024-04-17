e16563aedec31288206cbd6765eda8984fee7826
NicolasCardinaux <nicocardina01@gmail.com>
2024-04-17 19:17:38 -0300

    Update punt_3.py

--- TP1-TP2-TP3-TP4/src/TP3/punt_3.py
+++ TP1-TP2-TP3-TP4/src/TP3/punt_3.py
@@ -2,20 +2,34 @@
 pueda ser entregada en mostrador, retirada por el cliente o enviada por
 delivery. A los efectos prácticos bastará que la clase imprima el método de
 entrega'''
-
 class ComidaRapida:
-    def __init__(self, metodo_entrega):
-        self.metodo_entrega = metodo_entrega
-
     def entregar(self):
-        print(f'La comida será entregada por: {self.metodo_entrega}')
+        pass
 
+class EntregaMostrador(ComidaRapida):
+    def entregar(self):
+        print('La comida será entregada en el mostrador.')
+
+class EntregaCliente(ComidaRapida):
+    def entregar(self):
+        print('La comida será retirada por el cliente.')
+
+class EntregaDelivery(ComidaRapida):
+    def entregar(self):
+        print('La comida será enviada por delivery.')
 
 class ComidaRapidaFactory:
     @staticmethod
     def crear_comida_rapida(metodo_entrega):
-        return ComidaRapida(metodo_entrega)
-
+        if metodo_entrega == 'mostrador':
+            return EntregaMostrador()
+        elif metodo_entrega == 'cliente':
+            return EntregaCliente()
+        elif metodo_entrega == 'delivery':
+            return EntregaDelivery()
+        else:
+            print('Método de entrega no válido.')
+            return None
 
 # Uso de la clase
 factory = ComidaRapidaFactory()
@@ -25,5 +39,7 @@ opcion = input("Elija el método de entrega (mostrador, retirada por el cliente,
 
 # Creación de la comida rápida según la opción elegida por el usuario
 comida_rapida = factory.crear_comida_rapida(opcion)
-comida_rapida.entregar()  # Imprime el método de entrega elegido por el usuario
+if comida_rapida is not None:
+    comida_rapida.entregar()  # Imprime el método de entrega elegido por el usuario
+
 
