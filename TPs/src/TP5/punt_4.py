6dcf53237902877541ef6d7e48206f3f4b600e1c
NicolasCardinaux <nicocardina01@gmail.com>
2024-04-22 19:33:04 -0300

    Update punt_4.py

--- TPs/src/TP5/punt_4.py
+++ TPs/src/TP5/punt_4.py
@@ -2,7 +2,7 @@
 de barrido de radios que tiene incluya la sintonía de una serie de frecuencias
 memorizadas tanto de AM como de FM. Las frecuencias estarán etiquetadas como M1, M2, M3 y M4.
 Cada memoria podrá corresponder a una radio de AM o de FM en sus respectivas frecuencias específicas.
- En cada ciclo de barrido se barrerán las cuatro memorias.'''
+ En cada ciclo de barrido se barrerán las cuatro memorias'''
 import os
 
 class State:
